a18699fa9b61e4798598bd386a5d4a0d47e36bba
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2025-02-28 11:36:08 +0800

    finishing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { setJabatan } from '../utils/helper';
 import Button from './button/Button';
 import Profile from './Profile';
 
@@ -7,9 +8,12 @@ const Navbar = () => {
     window.location.href = '/dashboard';
   };
 
+  const token = localStorage.getItem('token');
+  const decoded = JSON.parse(atob(token.split('.')[1])); // Decode JWT payload
+
   return (
     <nav className='sticky top-0 flex items-center justify-between w-full px-5 py-2 bg-brand'>
-      <Profile nama={'Dede Setiawan'} jabatan={'Manager'} img={'/profile/man.png'} />
+      <Profile nama={decoded.nama} jabatan={setJabatan(decoded.access)} img={'/profile/man.png'} />
       <Button className={'text-light bg-brand'} text={'Log Out'} onClick={logout} />
     </nav>
   );
